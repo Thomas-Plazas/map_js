cae0c9d5d8545582a7e60ad4751603c9f9ca5a21
balmain <balmain@BALMAIN-2.home>
2021-01-04 14:07:09 +0100

    work

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,4 +1,23 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = 'green';
-ctx.fillRect(10,10,100,100);
+const HEXTILES_IMAGE = new Image();
+HEXTILES_IMAGE.src = 'img/fantasyhextiles_v3_borderless.png';
+Promise.all([
+    new Promise( (resolve) => {HEXTILES_IMAGE.addEventListener('load', () => { resolve();}); })
+])
+.then(() => {
+
+      for(var j=0;j<30;j++){
+         for(var i=0;i<10;i++){
+         var x;
+         if(j%2==0){
+            x=i*48+24;
+         }
+         else{
+            x=i*48;
+         }
+         ctx.drawImage(HEXTILES_IMAGE,Math.floor(Math.random() * (7 - 0) )*32,0,32,52,x,j*14,32,52);
+      }
+   }
+   //a
+});

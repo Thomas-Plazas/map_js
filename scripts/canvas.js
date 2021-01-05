b0e25571523b3d814d5eb33176c1e9fcdb68c7b8
balmain <balmain@BALMAIN-2.home>
2021-01-05 10:04:32 +0100

    all biomes

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -27,20 +27,26 @@ Promise.all([
 );
 
 function draw(hauteur,largeur){
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
    for(var j=0; j<hauteur; j++){
       for(var i=0;i<largeur;i++){
          var x=i*48;
          if(j%2==0){
-            x=x+24;
+            x=i*48+24;
          }
-         ctx.drawImage(HEXTILES_IMAGE,Math.floor(Math.random() * (7 - 0) )*32,0,32,52,x,j*14,32,52);
+         drawTuile(x,j,Math.floor(Math.random() * (41 - 0))+0);
+         //ctx.drawImage(HEXTILES_IMAGE,Math.floor(Math.random() * (7 - 0) )*32,0,32,52,x,j*14,32,52);
+
       }
    }
 }
 
 function getImgX(id){
-   var y=(id/8)-1;
+   return id%8;
 }
 function getImgY(id){
-   var y=(id/8)-1;
+   return parseInt(id/8);
+}
+function drawTuile(x,y,id){
+   ctx.drawImage(HEXTILES_IMAGE,getImgX(id)*32,getImgY(id)*48,32,48,x,y*14,32,48);
 }

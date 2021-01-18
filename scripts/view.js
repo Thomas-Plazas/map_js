563a55ec8c67db2ba7dcc83225c02aef54887ad4
Thomas <thomas.plazas.reau@gmail.com>
2021-01-18 16:27:48 +0100

    font

--- scripts/view.js
+++ scripts/view.js
@@ -32,14 +32,20 @@ class View {
         //this.ctx.drawImage(hextiles, img_x * 32, img_y * 48, 32, 48, x/2, y * 7, 16, 24);//36x
         this.ctx.drawImage(hextiles, img_x * 32, img_y * 48, 32, 48, x, y * 14, 32, 48);//18x62
     }
-    draw_text(x,y,text) {
-        this.ctx.font = "20px Arial";
+
+    draw_text(x, y, text) {
+        this.ctx.font = '20px BreatheFire';
         //this.ctx.fillStyle = "red";
         this.ctx.textAlign = "center";
-        this.ctx.fillText(text,x+18,y*14+60);
+        this.ctx.fillStyle = "black";
+        this.ctx.fillText(text, x + 18, y * 14 + 60);
+
+        this.ctx.strokeStyle = "black";
+        this.ctx.lineWidth = 0.2;
+        this.ctx.strokeText(text, x + 18, y * 14 + 60);
     }
 
-    refresh(){
-       this.get_ctx().clearRect(0, 0, this.get_canvas().width, this.get_canvas().height);
+    refresh() {
+        this.get_ctx().clearRect(0, 0, this.get_canvas().width, this.get_canvas().height);
     }
 }
